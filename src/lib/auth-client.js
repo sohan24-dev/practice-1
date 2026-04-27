97b6e5a7a9fe693809a6d431a3d84c7c01ed6f02
sohan24-dev <sohan.explorer@gmail.com>
2026-04-27 23:36:44 +0600

    add

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,8 +1,9 @@
 import { createAuthClient } from "better-auth/react"
-
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL
+    /** The base URL of the server (optional if you're using the same domain) */
+    baseURL: "http://localhost:3000"
 })
 
-// use from SAME instance
-export const { signIn, signUp, useSession } = authClient
+
+
+export const { signIn, signUp, useSession } = createAuthClient()
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -15,15 +15,13 @@ export const auth = betterAuth({
         enabled: true,
     },
     socialProviders: {
-        google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID, 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
-        }, 
-    },
-    socialProviders: {
         github: { 
             clientId: process.env.GITHUB_CLIENT_ID, 
             clientSecret: process.env.GITHUB_CLIENT_SECRET, 
         }, 
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+        },
     },
 });
