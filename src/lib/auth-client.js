e71fefb7a671b8837e472a3daf9f146f36688eac
sohan24-dev <sohan.explorer@gmail.com>
2026-04-27 23:15:50 +0600

    add

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,8 +1,8 @@
 import { createAuthClient } from "better-auth/react"
+
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL:process.env.BETTER_AUTH_URL
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL
 })
 
-
-export const { signIn, signUp, useSession } = createAuthClient()
+// use from SAME instance
+export const { signIn, signUp, useSession } = authClient
