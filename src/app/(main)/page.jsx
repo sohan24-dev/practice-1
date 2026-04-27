e675ec707e30f11f44da2f68ed84f9d3831b3896
sohan24-dev <sohan.explorer@gmail.com>
2026-04-27 22:25:12 +0600

    add

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="container mx-auto">
-      <h2 className="text-2xl text-center">This is homes page</h2>
+      <h2 className="text-2xl text-center">This is home page</h2>
     </div>
   );
 }
